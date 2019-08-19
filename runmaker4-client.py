1a76859bd1da1fd168a3f2a5652262965c4b8544
Michele Segata <segata@ccs-labs.org>
2019-08-19 13:42:14 +0200

    Make TCP client/server variant of runmaker public as well

---
+++ runmaker4-client.py
@@ -0,0 +1,309 @@
+#!/usr/bin/env python
+
+#
+# Copyright (C) 2012-2019 Christoph Sommer <christoph.sommer@uibk.ac.at>
+# Copyright (C) 2015 Michele Segata <segata@ccs-labs.org>
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+#
+# Executes jobs provided via network by runmaker4-server.py
+#
+
+import fcntl
+import os
+import select
+import signal
+import subprocess
+import sys
+import socket
+import multiprocessing
+import random
+import time
+from optparse import OptionParser
+
+LOGWIDTH = 500
+
+class Job:
+    """
+    Stores a (parsed) line in the job file.
+    """
+
+    number = 0
+    offset = 0
+    length = 0
+    state = "."
+    cmd = ""
+
+    def __repr__(self):
+        return "Job(%s, %s, '%s', '%s')" % (self.offset, self.length, self.state, self.cmd)
+
+
+def set_job_state(job, newstate, host, options):
+
+    #do 5 attempts
+    attempts = 5
+    while (attempts > 0):
+        attempts = attempts - 1
+        try:
+            #connect to the server
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sa = (host, options.port)
+            sock.connect(sa)
+            #send command to change job status
+            sock.sendall("SET " + options.token + " " + str(job.number) + " " + newstate)
+            #receive the ack, to be sure server go the message
+            sock.recv(2048)
+            #close the connection
+            sock.close()
+            return
+        except:
+            #if something went wrong, wait a little and try again
+            print "Error connecting to server. Retrying in a few seconds."
+            time.sleep(random.uniform(0,3))
+            continue
+
+    raise
+
+
+def run_job(job, options):
+    """
+    Fork and execute the job, wait for completion, return the exit code.
+    """
+
+    s = "executing `%s'" % job.cmd
+    print s
+
+    logf = None
+    log = [":".ljust(LOGWIDTH) for i in xrange(options.logfile_lines)]
+    if options.logfile:
+        s = ".-> %s (in %s)" % (job.cmd, os.getcwd())
+        s = "%s\n" % s[:LOGWIDTH].ljust(LOGWIDTH)
+        logf = open(options.logfile, 'rb+', 0)
+        logf.seek((job.number - 1) * (LOGWIDTH + 1) * (options.logfile_lines + 1))
+        logf.write(s)
+        for s in log:
+            logf.write("%s\n" % s)
+
+    opp = subprocess.Popen(job.cmd, shell=True, preexec_fn=os.setpgrp, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, )
+    try:
+        opp_pid = "%s,%s" % (os.uname()[1], opp.pid)
+        s = "status (%s): %s \"%s\"" % (opp_pid, "forked", job.cmd)
+        print s
+        if logf:
+            s = "+ %s" % s
+            log.pop(0)
+            log.append(s[:LOGWIDTH].ljust(LOGWIDTH))
+        opp.stdin.close()
+
+        poll = select.poll()
+        poll.register(opp.stdout, select.POLLIN | select.POLLHUP)
+        poll.register(opp.stderr, select.POLLIN | select.POLLHUP)
+        pollc = 2
+
+        events = poll.poll()
+        while pollc > 0 and len(events) > 0:
+            for event in events:
+                (rfd, event) = event
+                if event & select.POLLIN:
+                    if rfd == opp.stdout.fileno():
+                        line = opp.stdout.readline()
+                        if len(line) > 0:
+                            s = "stdout (%s): %s" % (opp_pid, line[:-1])
+                            if logf:
+                                s = ": %s" % s
+                                log.pop(0)
+                                log.append(s[:LOGWIDTH].ljust(LOGWIDTH))
+                            else:
+                                print s
+                    if rfd == opp.stderr.fileno():
+                        line = opp.stderr.readline()
+                        if len(line) > 0:
+                            s = "stderr (%s): %s" % (opp_pid, line[:-1])
+                            if logf:
+                                s = "! %s" % s
+                                log.pop(0)
+                                log.append(s[:LOGWIDTH].ljust(LOGWIDTH))
+                            else:
+                                print s
+                if event & select.POLLHUP:
+                    poll.unregister(rfd)
+                    pollc = pollc - 1
+                if logf:
+                    logf.seek((job.number - 1) * (LOGWIDTH + 1) * (options.logfile_lines + 1) + (LOGWIDTH + 1))
+                    for s in log:
+                        logf.write("%s\n" % s)
+                if pollc > 0:
+                    events = poll.poll()
+        returncode = opp.wait()
+        s = "status (%s): %s %s \"%s\"" % (opp_pid, "exit", returncode, job.cmd)
+        print s
+        if logf:
+            s = "+ %s" % s
+            log.pop(0)
+            log.append(s[:LOGWIDTH].ljust(LOGWIDTH))
+        if logf:
+            logf.seek((job.number - 1) * (LOGWIDTH + 1) * (options.logfile_lines + 1) + (LOGWIDTH + 1))
+            for s in log:
+                logf.write("%s\n" % s)
+        return returncode
+
+    except:
+        os.killpg(os.getpgid(opp.pid), signal.SIGINT)
+        raise
+
+    finally:
+        if logf:
+            logf.close()
+
+def process_file(host, options):
+    """
+    Open the job file, and for each job to be executed, execute it.
+    """
+    run = True
+    lastException = 0
+    while run:
+        #do 5 attempts for each request, to be sure to avoid problems due to
+        #concurrent with the others
+        attempts = 5
+        while (attempts > 0):
+            attempts = attempts - 1
+            job_done = False
+            try:
+                #connect to server
+                sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                sa = (host, options.port)
+                sock.connect(sa)
+
+                #ask for a job
+                sock.sendall("GET " + options.token)
+                response = sock.recv(2048)
+                if (response == ""):
+                    print "Empty server response"
+                    time.sleep(random.uniform(0,3))
+                    continue
+                if (response == "INVALID_CMD"):
+                    print "Got invalid command error from server. Check the code. Quitting"
+                    sys.exit(1)
+                if (response == "INVALID_TOKEN"):
+                    print "Got invalid token error. Check that token or token file are correct. Quitting"
+                    sys.exit(1)
+
+                sock.sendall("ACK")
+                #job looks like "JOBID CMD"
+                v = response.split(" ", 1)
+
+                job = Job()
+                job.number = int(v[0])
+                if (job.number != -1):
+                    job.cmd = v[1]
+
+                    #run the job
+                    try:
+                        set_job_state(job, 'r', host, options)
+                        if run_job(job, options) == 0:
+                            job_done = True
+                            set_job_state(job, 'd', host, options)
+                        else:
+                            set_job_state(job, '!', host, options)
+                    except KeyboardInterrupt as ki:
+                        #if the user hits ctrl-c, set job status to e, and exit
+                        set_job_state(job, 'e', host, options)
+                        return False
+
+                else:
+                    #server said there's nothing left to do. stop here
+                    return True
+
+            except Exception as ex:
+                #if we got an exception, sleep for a random amount of time
+                #then ask again
+                print "Exception caught. Retrying in a few seconds."
+                lastException = ex
+                time.sleep(random.uniform(0,3))
+                continue
+
+        #when the number of attempts goes to 0, then something bad is going on. stop everything
+        if (not job_done and attempts == 0):
+            print "Job quitting because of consecutive errors."
+            if (lastException != 0):
+                print "Last exception:"
+                print lastException
+            run = False
+
+
+def main():
+    """
+    Program entry point when run interactively.
+    """
+
+    # prepare option parser
+    parser = OptionParser(usage="usage: %prog [options] host", description="Run", epilog="Refer to the help output of runmaker4-server.py for more details.")
+    parser.add_option("-j", "--jobs", dest="num_jobs", type="int", default=1, action="store", help="start NUMBER jobs in parallel, 0 meaning autodetect [default: %default]", metavar="NUMBER")
+    parser.add_option("-l", "--logfile", dest="logfile", default="", help="log output to FILENAME [default: none]", metavar="FILENAME")
+    parser.add_option("-n", "--loglines", dest="logfile_lines", type="int", default=3, action="store", help="if logging, log the last NUMBER lines of output [default: %default]", metavar="NUMBER")
+    parser.add_option("-p", "--port", dest="port", type="int", default=9998, action="store", help="TCP PORT the server is listening to [default: %default]", metavar="PORT")
+    parser.add_option("-t", "--token", dest="token", default=os.path.join(os.path.expanduser("~"), ".runmaker4.token"), action="store", help="string representing either the token or the file where the token is stored. The token is sent to the server at each request for authentication purpose. If the parameter ends with .token, it is assumed that the token needs to be read from a file. [default: %default]")
+
+    # parse options
+    (options, args) = parser.parse_args()
+
+    if options.logfile:
+        if (not os.path.exists(options.logfile)) or (not os.path.isfile(options.logfile)):
+            print "You need to create log file %s before starting runmaker4-client. Stop" % options.logfile
+            sys.exit(1)
+
+    # get file name
+    if len(args) != 1:
+        print "Need host address or name"
+        print ""
+        print parser.get_usage()
+        sys.exit(1)
+    host = args[0]
+
+    if (options.token.endswith(".token")):
+        #we need to take the token from a file
+        try:
+            tokenFile = open(options.token, 'r')
+            #overwrite the filename with the token. we'll pass this to the subprocesses
+            options.token = tokenFile.read()
+            tokenFile.close()
+        except:
+            print ("Error occured while retrieving the token. Does the token file exist?")
+            sys.exit(1)
+
+    # autodetect number of cpus
+    if options.num_jobs == 0:
+        try:
+            options.num_jobs = multiprocessing.cpu_count()
+        except:
+            pass
+
+    # spawn children
+    children = []
+    for i in range(options.num_jobs):
+        child = multiprocessing.Process(target=process_file, args=(host,options))
+        child.start()
+        children.append(child)
+    for child in children:
+        child.join()
+
+# Start main() when run interactively
+if __name__ == '__main__':
+    main()
+
---
+++ runmaker4-server.py
@@ -0,0 +1,319 @@
+#!/usr/bin/env python
+
+#
+# Copyright (C) 2012-2019 Christoph Sommer <christoph.sommer@uibk.ac.at>
+# Copyright (C) 2015 Michele Segata <segata@ccs-labs.org>
+#
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+#
+
+#
+# Waits for connections from runmaker4.py asking for a job to perform
+#
+
+import fcntl
+import os
+import select
+import signal
+import subprocess
+import sys
+import socket
+import multiprocessing
+import tempfile
+import logging
+import string
+import random
+from optparse import OptionParser
+
+class Job:
+    """
+    Stores a (parsed) line in the job file.
+    """
+
+    number = 0
+    offset = 0
+    length = 0
+    state = "."
+    cmd = ""
+
+    def __repr__(self):
+        return "Job(%s, %s, '%s', '%s')" % (self.offset, self.length, self.state, self.cmd)
+
+class Command:
+    #definition of constants
+    #commands
+    CMD_UNINITIALIZED = -1
+    CMD_GET           = 0
+    CMD_SET           = 1
+    #errors/results
+    VALID_CMD     = 0
+    INVALID_CMD   = -1
+    INVALID_TOKEN = -2
+
+    #variables
+    command = CMD_UNINITIALIZED
+    parseResult = INVALID_CMD
+    token = ""
+    jobNumber = -1
+    jobStatus = -1
+
+
+def read_jobs(f):
+    """
+    Read the job file, return the parsed list of jobs.
+    """
+
+    jobs = []
+
+    f.seek(0)
+    while 1:
+        job = Job()
+        job.number = len(jobs)+1
+        job.offset = f.tell()
+        s = f.readline()
+        job.length = f.tell() - job.offset
+        if not s:
+            break
+        if len(s) < 3:
+            continue
+        if (s[0] == "#" or s[0] == "/"):
+            continue
+        # line format: <state><one whitespace><commandline>
+        if not (s[1] == "\t" or s[1] == " "):
+            continue
+        s = s.rstrip()
+        job.state = s[0]
+        job.cmd = s[2:]
+
+        jobs.append(job)
+
+    return jobs
+
+
+def set_job_state(f, job, newstate):
+    """
+    Do four things:
+    - Make sure the job file matches a job object.
+    - Modify the job file to reflect a job's new state.
+    - Modify a job object to reflect its new state.
+    - Return true if successful.
+    """
+
+    assert(not f.closed)
+    assert(job.length > 0)
+    assert(len(newstate) == 1)
+
+    f.seek(job.offset)
+    s = f.read(1)
+    if s != job.state:
+       return False
+    f.seek(job.offset)
+    f.write(newstate)
+    f.flush()
+
+    job.state = newstate
+
+    return True
+
+def get_new_job(jobs, f, options):
+    for job in jobs:
+        # keep going until we find a pristine job
+        if not ((job.state == '.') or (options.retry and (job.state == '!' or job.state == 'e'))):
+            continue
+        # try to claim the job
+        if not set_job_state(f, job, '?'):
+            continue
+
+        return job
+
+    job = Job()
+    job.number = -1
+    job.cmd = ""
+    return job
+
+def parse_command(command, token, options):
+
+    #command to be returned. invalid by default
+    cmd = Command()
+    #split command in parts
+    parts = command.split(" ")
+
+    if (len(parts) == 0):
+        return cmd
+
+    if (parts[0] == "GET"):
+        #GET format is GET <token>
+        if (len(parts) != 2):
+            return cmd
+        if (parts[1].strip() != token):
+            cmd.parseResult = Command.INVALID_TOKEN
+            return cmd
+        cmd.command = Command.CMD_GET
+        cmd.parseResult = Command.VALID_CMD
+        return cmd
+    elif (parts[0] == "SET"):
+        #SET format is SET <token> <job number> <job status>
+        if (len(parts) != 4):
+            return cmd
+        if (parts[1].strip() != token):
+            cmd.parseResult = Command.INVALID_TOKEN
+            return cmd
+        try:
+            cmd.jobNumber = int(parts[2])
+        except:
+            #job number is not a valid integer
+            return cmd
+
+        if (not (parts[3] in ['r', 'd', 'e', '!'])):
+            return cmd
+
+        cmd.jobStatus = parts[3]
+        cmd.command = Command.CMD_SET
+        cmd.parseResult = Command.VALID_CMD
+        return cmd
+    else:
+        return cmd
+
+
+def process_get(jobs, f, client, options, client_address):
+    #get a job yet to be done
+    job = get_new_job(jobs, f, options)
+    logging.debug(str(client_address) + " Returning job number " + str(job.number) + " command: " + job.cmd)
+    #return the client the id of the job and the command to execute
+    client.sendall(str(job.number) + " " + job.cmd)
+    client.recv(2048)
+
+def process_set(jobs, f, client, options, jobn, state, client_address):
+    #get all jobs and search for the job requested by the client
+    for job in jobs:
+        if (job.number == jobn):
+            #set the state to the required value
+            logging.debug(str(client_address) + " Setting job number " + str(job.number) + " status to " + state)
+            set_job_state(f, job, state)
+            break
+
+
+def main():
+    """
+    Program entry point when run interactively.
+    """
+
+    # prepare option parser
+    parser = OptionParser(usage="usage: %prog [options] filename", description="Read a text file with jobs, execute them one by one.", epilog="In the given file, each line beginning with a dot and a space (. ) will be executed. The file is modified to reflect the execution state of each job (r-running, d-done, !-failed, e-error).")
+    parser.add_option("-r", "--retry", dest="retry", default=False, action="store_true", help="retry failed jobs [default: no]")
+    parser.add_option("-l", "--logfile", dest="logfile", default=os.path.join(tempfile.gettempdir(), "runmaker4-server.log"), help="log output to FILENAME [default: %default]", metavar="FILENAME")
+    parser.add_option("-v", "--verbose", dest="count_verbose", default=0, action="count", help="increase verbosity [default: don't log infos, debug]")
+    parser.add_option("-q", "--quiet", dest="count_quiet", default=0, action="count", help="decrease verbosity [default: log warnings, errors]")
+    parser.add_option("-p", "--port", dest="port", type="int", default=9998, action="store", help="TCP PORT the         server has to listen to [default: %default]", metavar="PORT")
+    parser.add_option("-d", "--daemon", dest="daemonize", default=False, action="store_true", help="detach and run as daemon [default: no]")
+    parser.add_option("-t", "--tokenfile", dest="tokenfile", default=os.path.join(os.path.expanduser("~"), ".runmaker4.token"), action="store", help="string representing the file where the token is stored [default: %default]")
+
+    # parse options
+    (options, args) = parser.parse_args()
+
+    _LOGLEVELS = (logging.ERROR, logging.WARN, logging.INFO, logging.DEBUG)
+    loglevel = _LOGLEVELS[max(0, min(1 + options.count_verbose - options.count_quiet, len(_LOGLEVELS)-1))]
+
+    # get file name
+    if len(args) != 1:
+        print "Need exactly one filename (a list of all jobs to run)"
+        print ""
+        print parser.get_usage()
+        sys.exit(1)
+    fname = args[0]
+
+    logging.basicConfig(filename=options.logfile, level=loglevel)
+    if not options.daemonize:
+        logging.getLogger().addHandler(logging.StreamHandler())
+    else:
+        print("The --daemon option is not implemented.")
+    logging.debug("Logging to %s" % options.logfile)
+
+    f = open(fname, 'rb+', 0)
+    jobs = read_jobs(f)
+
+    tokenSize=6
+    tokenChars=string.ascii_uppercase + string.digits
+    token = ''.join(random.choice(tokenChars) for _ in range(tokenSize))
+    print "Token for runmaker4-client.py: %s (written to %s)" % (token, options.tokenfile)
+    if (options.tokenfile != ""):
+        #we need to write the token to a file
+        with os.fdopen(os.open(options.tokenfile, os.O_WRONLY | os.O_CREAT, 0600), 'w') as handle:
+            handle.write(token)
+
+    #create a socket and start listening
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_address = ('0.0.0.0', options.port)
+    logging.debug("Starting runmaker4-server.py on port " + str(options.port))
+    sock.bind(server_address)
+
+    sock.listen(0)
+    run = True
+    while run:
+        try:
+            #accept incoming connection. note that we don't use threads
+            #we serve one client at a time, thus automatically synchronizing clients
+            client, client_address = sock.accept()
+
+            logging.debug("Connection from " + str(client_address))
+            data = client.recv(2048).rstrip()
+
+            cmd = parse_command(data, token, options)
+
+            if cmd.parseResult == Command.INVALID_CMD:
+                client.sendall("INVALID_CMD")
+                logging.error(str(client_address) +  " Received invalid command: " + data)
+            elif cmd.parseResult == Command.INVALID_TOKEN:
+                client.sendall("INVALID_TOKEN")
+                logging.error(str(client_address) + " Received invalid token. Ignoring request: " + data)
+            else:
+                if cmd.command == Command.CMD_GET:
+                    process_get(jobs, f, client, options, client_address)
+                elif cmd.command == Command.CMD_SET:
+                    process_set(jobs, f, client, options, cmd.jobNumber, cmd.jobStatus, client_address)
+                    client.sendall("ACK")
+
+            client.close()
+
+        except SystemExit:
+            run = False
+            logging.debug("Killed.")
+
+        except KeyboardInterrupt:
+            run = False
+            logging.debug("Keyboard interrupt.")
+
+        except:
+            raise
+
+    # clean up
+    logging.debug("Shutting down.")
+    if (options.tokenfile != ""):
+        os.remove(options.tokenfile)
+    sock.close()
+
+    f.close()
+
+def signal_handler(signal, frame):
+    sys.exit(1)
+
+# Start main() when run interactively
+if __name__ == '__main__':
+    signal.signal(signal.SIGTERM, signal_handler)
+    main()
+
